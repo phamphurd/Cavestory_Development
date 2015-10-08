54b69024c1d53e3b83be26915b39c7e0dfadfbd4
Phamphurd <phamphurd@users.noreply.github.com>
2015-10-07 23:56:09 -0500

    Remaking Cavestory in C++ - Episode 3: Part 2, Adding input capture to game class

--- game.cpp
+++ game.cpp
@@ -18,42 +18,31 @@ Game::~Game(){
 }
 
 void Game::gameLoop(){
-      Graphics graphics; //object
-      SDL_Event event;   //SDL event object
+    Graphics graphics; //object
+    Input input;
+    SDL_Event event;   //SDL event object
     
-      while (true){
-          if(SDL_PollEvent(&event)){
-              if(event.type == SDL_QUIT){
-                  return;
-              }
-          }
-      }
+    while(true){
+        input.beginNewFrame();
+        
+        if(SDL_PollEvent(&event)){
+            if(event.type == SDL_KEYDOWN){
+                if(event.key.repeat == 0){
+                    input.keyDownEvent(event);
+                }
+            }
+            else if (event.type == SDL_KEYUP){
+                input.keyUpEvent(event);
+            }
+            else if (event.type == SDL_QUIT){
+                return;
+            }
+        }
+        if (input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true){
+            return;
+        }
+    }
 }
-//    Graphics graphics; //object
-//    Input input;
-//    SDL_Event event;   //SDL event object
-//    
-//    while(true){
-//        input.beginNewFrame();
-//        
-//        if(SDL_PollEvent(&event)){
-//            if(event.type == SDL_KEYDOWN){
-//                if(event.key.repeat == 0){
-//                    input.keyDownEvent(event);
-//                }
-//            }
-//            else if (event.type == SDL_KEYUP){
-//                input.keyUpEvent(event);
-//            }
-//            else if (event.type == SDL_QUIT){
-//                return;
-//            }
-//        }
-//        if (input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true){
-//            return;
-//        }
-//    }
-//}
 
 void Game::draw(Graphics& graphics){
     
